13db5b3a4b62696fc48c32696ef74facdaf90885
ValerioPirolli <pirollivalerio@gmail.com>
2023-12-06 15:48:20 +0100

    home fatta

--- REST-Countries-API/src/assets/components/Home.tsx
+++ REST-Countries-API/src/assets/components/Home.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { flagsDataStore } from "../stores/flagsDataStore";
+import { Flag } from "./flag";
+import { TState } from "../models/TState";
 
 export const Home = () => {
   const { setAllStates, allStates } = flagsDataStore();
@@ -7,9 +9,9 @@ export const Home = () => {
     setAllStates();
   }, [setAllStates]);
   return (
-    <div>
-      {allStates.map((step) => (
-        <h1>name: {step.name}</h1>
+    <div className="w-3/4 mx-auto flex flex-col gap-9">
+      {allStates.map((state: TState) => (
+        <Flag {...state} />
       ))}
     </div>
   );
--- REST-Countries-API/src/assets/components/flag.tsx
+++ REST-Countries-API/src/assets/components/flag.tsx
@@ -1,3 +1,21 @@
-export const flag = () => {
-  return <></>;
+import { TState } from "../models/TState";
+
+export const Flag = (data: TState) => {
+  return (
+    <div className="bg-white rounded-lg overflow-hidden shadow-md">
+      <img src={data.flags.svg} className="shadow-md" />
+      <div className="px-6 pb-10 leading-7 pt-4">
+        <h3 className="font-  text-xl text my-3">{data.name}</h3>
+        <p className="font-semibold">
+          Population: <span className="font-normal">{data.population}</span>
+        </p>
+        <p className="font-semibold">
+          Region: <span className="font-normal">{data.region}</span>
+        </p>
+        <p className="font-semibold">
+          Capital: <span className="font-normal">{data.capital}</span>
+        </p>
+      </div>
+    </div>
+  );
 };
